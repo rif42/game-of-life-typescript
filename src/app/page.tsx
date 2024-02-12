36eeee6213865e65e4c6528484c82226a9109ebb
rifky <ee.guy.42@gmail.com>
2024-02-12 15:55:35 +0700

    feat: finished adding check valid indexes in surrounding target index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,26 +30,113 @@ export default function Home() {
             }
         }
         toast.success("Matrix Changed");
-        const xValue = xnumber.current ? parseInt(xnumber.current.value) : 1;
-        const yValue = ynumber.current ? parseInt(ynumber.current.value) : 1;
+        const xValue: number = xnumber.current ? parseInt(xnumber.current.value) : 1;
+        const yValue: number = ynumber.current ? parseInt(ynumber.current.value) : 1;
         setxMatrix(xValue > 1 ? xValue : 1);
         setyMatrix(yValue > 1 ? yValue : 1);
         createMatrix(xValue, yValue);
     }
 
-    function handleMutate(index: number) {
-        const newMatrix = [...matrix];
-        if (newMatrix[index] === 1) newMatrix[index] = 0;
+    function handleMutate(matrix_index: number) {
+        const newMatrix: number[] = [...matrix];
+        if (newMatrix[matrix_index] === 1) newMatrix[matrix_index] = 0;
         else {
-            newMatrix[index] = 1;
+            newMatrix[matrix_index] = 1;
         }
         setMatrix(newMatrix);
+        // console.log(checkValidIndexes(matrix_index));
     }
 
-    useEffect(() => {
-        console.log(`xMatrix: ${xMatrix}, yMatrix: ${yMatrix}`);
-        console.log(`Matrix : ${matrix}`);
-    }, [xMatrix, yMatrix, matrix]);
+    function generateNext(matrix_index: number) {
+        const valid_matrix: number[] = checkValidIndexes(matrix_index);
+        const buffer_matrix: number[] = [];
+        const edge_matrix: number[] = [];
+        matrix.forEach((val, index) => {
+            if (index === matrix_index) {
+                console.log("Left", isLeftEdgeMatrix(index));
+                console.log("Right", isRightEdgeMatrix(index));
+                console.log("Top", isTopEdgeMatrix(index));
+                console.log("Bottom", isBottomEdgeMatrix(index));
+            }
+        });
+    }
+
+    function checkValidIndexes(matrix_index: number): number[] {
+        const valid_indexes: number[] = [];
+        const remove_indexes: number[] = [];
+        for (let i = -1; i < 2; i++) {
+            valid_indexes.push(matrix_index - i - xMatrix);
+            valid_indexes.push(matrix_index - i);
+            valid_indexes.push(matrix_index - i + xMatrix);
+        }
+
+        if (isLeftEdgeMatrix(matrix_index) == true) {
+            valid_indexes.push(matrix_index - 1 - xMatrix);
+            valid_indexes.push(matrix_index - 1);
+            valid_indexes.push(matrix_index - 1 + xMatrix);
+        }
+
+        if (isTopEdgeMatrix(matrix_index) == true) {
+            valid_indexes.push(matrix_index - 1 - xMatrix);
+            valid_indexes.push(matrix_index - xMatrix);
+            valid_indexes.push(matrix_index + 1 - xMatrix);
+        }
+
+        if (isLeftEdgeMatrix(matrix_index) == true) {
+            valid_indexes.push(matrix_index + 1 - xMatrix);
+            valid_indexes.push(matrix_index + 1);
+            valid_indexes.push(matrix_index + 1 + xMatrix);
+        }
+
+        if (isBottomEdgeMatrix(matrix_index) == true) {
+            valid_indexes.push(matrix_index - 1 + xMatrix);
+            valid_indexes.push(matrix_index - xMatrix);
+            valid_indexes.push(matrix_index + 1 + xMatrix);
+        }
+
+        let final_index = valid_indexes.filter(
+            (item) => !remove_indexes.includes(item) && item >= 0 && item != matrix_index
+        );
+        final_index = final_index.filter((item, index) => final_index.indexOf(item) === index);
+        return final_index;
+    }
+
+    function isLeftEdgeMatrix(matrix_index: number) {
+        if (matrix_index % xMatrix) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function isRightEdgeMatrix(matrix_index: number) {
+        if ((matrix_index + 1) % xMatrix) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function isTopEdgeMatrix(matrix_index: number) {
+        if (matrix_index - xMatrix < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function isBottomEdgeMatrix(matrix_index: number) {
+        if (matrix_index + xMatrix >= xMatrix * yMatrix) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // useEffect(() => {
+    //     console.log(`xMatrix: ${xMatrix}, yMatrix: ${yMatrix}`);
+    //     console.log(`Matrix : ${matrix}`);
+    // }, [xMatrix, yMatrix, matrix]);
 
     return (
         <>
